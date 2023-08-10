3bd843639285296b236247bcfe84f6020855bc61
Uday Modala <126262658+udaymodala@users.noreply.github.com>
2023-08-10 20:14:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+for(var i=0;i<7;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+
+        var exp=this.innerHTML;
+        fun(exp);
+        animation(exp);
+    });
+}
+function fun(k){
+    switch(k){
+        case "w":
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    }
+}
+document.addEventListener("keypress",function(event){
+    fun(event.key);
+    animation(event.key);
+})
+function animation(keypressed){
+    document.querySelector("."+keypressed).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+keypressed).classList.remove("pressed");
+    },200)
+}
